10b128b33bdcc082be1112b0dd78a411fdd51f34
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-21 11:12:23 +0900

    first commit

---
+++ init.sh
@@ -0,0 +1,6 @@
+echo "# ShellScript" >> README.md
+git add README.md
+git commit -m "initial commit"
+git branch -M main
+git remote add origin https://github.com/jeongjiwoo0522/ShellScript.git
+git push -u origin main 
